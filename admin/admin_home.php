69561b6194e24d3a1d4dd3ae2d9a05832c0168be
prc16 <81236125+prc16@users.noreply.github.com>
2023-12-28 20:19:20 +0530

    Fixed titles of:
        index.html
        login.html
        signup.html
        admin_login.html
        admin_home.php

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create/Delete Table</title>
+    <title>Admin home</title>
 </head>
 <body>
 
