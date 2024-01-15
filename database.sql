846b18797856ae4166b76a486014687fe9be77bc
prc16 <81236125+prc16@users.noreply.github.com>
2024-01-15 18:34:15 +0530

    Update database column
    Renamed database column 'username' to 'alias'

--- database.sql
+++ database.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Jan 09, 2024 at 01:24 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+05:30";
@@ -19,9 +10,9 @@ USE `whisper_db`;
 
 
 CREATE TABLE `users` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  `password_hash` varchar(60) NOT NULL,
-  PRIMARY KEY (`uid`)
+    `uid` int(11) NOT NULL AUTO_INCREMENT,
+    `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    `password_hash` varchar(60) NOT NULL,
+    `alias` varchar(20) DEFAULT NULL,
+    PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='users';
