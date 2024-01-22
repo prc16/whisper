86117a985cfb618ef9254d9ffa05114c8d5c467b
prc16 <81236125+prc16@users.noreply.github.com>
2024-01-22 20:53:38 +0530

    Path changes
    Project can be cloned to C:\xampp\htdocs or C:\xampp\htdocs\whisper

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/home/');
+	header('Location: ./home/');
 	exit;
 ?>
