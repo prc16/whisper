15eacd3fc47d737132980c96ba966e8359dc8b43
prc16 <81236125+prc16@users.noreply.github.com>
2024-01-29 17:33:09 +0530

    database.sql comments

--- database.sql
+++ database.sql
@@ -1,14 +1,20 @@
+-- @block phpmyadmin stuff
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+05:30";
 
+-- --------------------------------------------------------
+
+-- @block Database: whisper_db
 
 DROP DATABASE IF EXISTS whisper_db;
 
-CREATE DATABASE IF NOT EXISTS `whisper_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+CREATE DATABASE `whisper_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 USE `whisper_db`;
 
+-- --------------------------------------------------------
 
+-- @block Table: `users`
 CREATE TABLE `users` (
     id INT AUTO_INCREMENT,
     user_id VARCHAR(50) UNIQUE NOT NULL,
@@ -17,6 +23,9 @@ CREATE TABLE `users` (
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='users';
 
+-- --------------------------------------------------------
+
+-- @block Table: posts
 CREATE TABLE `posts` (
     id INT AUTO_INCREMENT,
     user_id VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_bin,
@@ -26,3 +35,5 @@ CREATE TABLE `posts` (
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='posts';
+
+-- --------------------------------------------------------
