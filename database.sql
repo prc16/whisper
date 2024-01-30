bc9eda4e8082bf8277d8d1eed3485cfe8e82dd38
prc16 <81236125+prc16@users.noreply.github.com>
2024-01-30 19:55:30 +0530

    Adding comments

--- database.sql
+++ database.sql
@@ -1,4 +1,5 @@
 -- @block phpmyadmin stuff
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+05:30";
@@ -8,13 +9,13 @@ SET time_zone = "+05:30";
 -- @block Database: whisper_db
 
 DROP DATABASE IF EXISTS whisper_db;
-
 CREATE DATABASE `whisper_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 USE `whisper_db`;
 
 -- --------------------------------------------------------
 
 -- @block Table: `users`
+
 CREATE TABLE `users` (
     id INT AUTO_INCREMENT,
     user_id VARCHAR(50) UNIQUE NOT NULL,
@@ -26,6 +27,7 @@ CREATE TABLE `users` (
 -- --------------------------------------------------------
 
 -- @block Table: posts
+
 CREATE TABLE `posts` (
     id INT AUTO_INCREMENT,
     user_id VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_bin,
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+	// Setting the header to redirect the user to the ./home/ page
 	header('Location: ./home/');
 	exit;
 ?>
