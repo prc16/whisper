ebb5148b1b50c8781fe768b59fc4fdea98979046
prc16 <81236125+prc16@users.noreply.github.com>
2024-02-02 17:08:29 +0530

    database and code optimization
    * unsigned integers for primary keys
    * PRIMARY KEY (`user_id`, `post_id`) instead of id for votes table
    * "ON DUPLICATE KEY UPDATE vote_type = VALUES(vote_type)" when inserting into votes,
    * optimizes vote management.
    * SQL prepared statements
    * comments and documentation

--- database.sql
+++ database.sql
@@ -12,7 +12,7 @@ USE `whisper_db`;
 
 -- @block Create Table: users
 CREATE TABLE `users` (
-    id INT AUTO_INCREMENT,
+    id INT UNSIGNED AUTO_INCREMENT,
     user_id VARCHAR(50) UNIQUE NOT NULL,
     `email` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
     `password_hash` VARCHAR(60) NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE `users` (
 
 -- @block Create Table: posts
 CREATE TABLE `posts` (
-    id INT AUTO_INCREMENT,
+    id INT UNSIGNED AUTO_INCREMENT,
     user_id VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_bin,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
@@ -34,12 +34,10 @@ CREATE TABLE `posts` (
 
 -- @block Create Table: votes
 CREATE TABLE `votes` (
-    id INT AUTO_INCREMENT,
     user_id VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_bin,
-    post_id INT,
+    post_id INT UNSIGNED,
     vote_type ENUM('upvote', 'downvote') NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY `unique_vote` (`user_id`, `post_id`, `vote_type`),
+    PRIMARY KEY (`user_id`, `post_id`),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (post_id) REFERENCES posts(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='votes';
