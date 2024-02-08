b24fa2a380018345b49d54a9e99e2bfe9f668573
keval008 <147927821+keval008@users.noreply.github.com>
2024-02-08 14:47:00 +0530

    fixed sidebars in pages

--- home/index.php
+++ home/index.php
@@ -7,10 +7,15 @@
     <link rel="stylesheet" href="home.css">
     <link rel="stylesheet" href="../navbar/navbar.css">
     <link rel="stylesheet" href="../posts/posts.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="../posts/script.js"></script>
 </head>
 <body>
     <?php include '../navbar/navbar.php'; ?>
-    <?php include '../posts/posts.php'; ?>
+    <div class="web-container">
+    <div class="left-sidebar"></div>
+    <div class="main-content"><?php include '../posts/posts.php'; ?></div>
+    <div class="right-sidebar"></div>
+    </div>
 </body>
 </html>
