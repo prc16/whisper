ce11d489572f620fdce8d56a384d404bf5bcdc69
prc16 <81236125+prc16@users.noreply.github.com>
2024-02-11 13:02:04 +0530

    Layout changes
    * Removed navbar
    * moved sidebars to separate folders

---
+++ sidebar-left/content.php
@@ -0,0 +1,15 @@
+<div class="logo-link">
+    <img src="../images/whisper-logo.png" alt="Logo" class="logo">
+    <h1 class="title">Whisper</h1>
+</div>
+
+<div class="imp-links">
+    <a href="#"><i class="fa-solid fa-house"></i>home</a></br>
+    <a href="#"><i class="fa-solid fa-user"></i>profile</a></br>
+    <a href="#"><i class="fa-solid fa-user-group"></i></i>Friends</a></br>
+    <a href="#"><i class="fa-solid fa-envelope"></i>Messages</a></br>
+</div>
+
+<div class="bottom-left">
+    <?php include "../bottom-left/content.php"; ?>
+</div>
