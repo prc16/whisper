adfe01e513d13c958deeac0b52dcf29fe39d92f9
prc16 <81236125+prc16@users.noreply.github.com>
2024-02-14 00:05:17 +0530

    Unconditional session start

--- posts/server.php
+++ posts/server.php
@@ -3,10 +3,7 @@
 include '../database/config.php';
 $conn = getDBConnection();
 
-// Start the session if not already started
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
+session_start();
 
 /**
  * Function to get posts with user votes
