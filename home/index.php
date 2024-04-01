568118a9799ba732328160919c728dfea61642fa
prc16 <81236125+prc16@users.noreply.github.com>
2024-04-01 20:53:18 +0530

    home page layout change

--- home/index.php
+++ home/index.php
@@ -5,38 +5,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Whisper - Home</title>
-    
-    <link rel="stylesheet" href="../home/home.css">
+
+    <link rel="stylesheet" href="../home/style.css">
     <link rel="stylesheet" href="../sidebar-left/style.css">
     <link rel="stylesheet" href="../bottom-left/style.css">
     <link rel="stylesheet" href="../sidebar-right/style.css">
-    <link rel="stylesheet" href="../posts/posts.css">
-    
+    <link rel="stylesheet" href="../posts/style.css">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    
+
     <link rel="icon" type="image/gif" href="../images/whisper-logo-square.png">
-    
+
     <script src="../posts/script.js"></script>
+
 </head>
 
 <body>
     <div class="web-container">
         <!--------------- left sidebar --------------->
-        <div class="left-sidebar">
+        <header class="left-sidebar">
             <?php include_once '../sidebar-left/content.php'; ?>
-        </div>
+        </header>
 
         <!--------------- main content--------------->
-        <div class="main-content">
-            <?php include_once '../posts/content.php'; ?>
-        </div>
+        <main class="main-content">
 
-        <!--------------- right sidebar --------------->
-        <div class="right-sidebar">
-            <?php include_once '../sidebar-right/content.php'; ?>
-        </div>
+            <!--------------- home feed --------------->
+            <div class="home-feed">
+                <?php include_once '../posts/content.php'; ?>
+            </div>
 
+            <!--------------- right sidebar --------------->
+            <div class="right-sidebar">
+                <?php include_once '../sidebar-right/content.php'; ?>
+            </div>
+        </main>
     </div>
+
 </body>
 
 </html>
