04236be1b5affe424e0275be5f2aca571e5c74d4
prc16 <81236125+prc16@users.noreply.github.com>
2024-04-08 01:01:13 +0530

    Vote button css change

--- posts/script.js
+++ posts/script.js
@@ -29,8 +29,8 @@ function displayPosts(posts) {
                     </div>
                     <div id="displayPostButtons">
                         <p class="voteCount"> Votes: ${post.vote_count}</p>
-                        <button class="vote-btn btn" data-id="${post.post_id}" data-type="upvote" style="background-color: ${post.vote_type === 'upvote' ? 'var(--whisper-color);' : 'gray'}">Upvote</button>
-                        <button class="vote-btn btn" data-id="${post.post_id}" data-type="downvote" style="background-color: ${post.vote_type === 'downvote' ? 'var(--whisper-color);' : 'gray'}">Downvote</button>
+                        <button class="vote-btn btn ${post.vote_type === 'upvote' ? '' : 'btn-alt'}" data-id="${post.post_id}" data-type="upvote">Upvote</button>
+                        <button class="vote-btn btn ${post.vote_type === 'downvote' ? '' : 'btn-alt'}" data-id="${post.post_id}" data-type="downvote">Downvote</button>
                     </div>
                 </div>
             </div>
