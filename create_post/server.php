53ce10e2507aeb3f4011efbe3f8a2d14db00e210
prc16 <81236125+prc16@users.noreply.github.com>
2024-04-09 17:53:52 +0530

    removed image conversion due to gd not being enabled in xampp by default

--- create_post/server.php
+++ create_post/server.php
@@ -10,11 +10,13 @@ session_start();
 // Check if the user is logged in
 if (!isset($_SESSION['user_id'])) {
     errorResponse(401, 'You need to log in to create post');
+    exit;
 }
 
 // Validate the Request
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     errorResponse(400, 'Bad Request');
+    exit;
 }
 
 $content = trim($_POST['post_text']);
@@ -27,57 +29,39 @@ if (isset($_FILES['media_file'])) {
 
     // Generate a unique filename using genUUID() function
     $media_file_id = genUUID();
-    $media_file_ext = "jpeg";
+    $media_file_ext = strtolower(pathinfo($_FILES["media_file"]["name"], PATHINFO_EXTENSION));
     $target_file = $target_dir . $media_file_id . '.' . $media_file_ext;
-
-    $imageFileType = strtolower(pathinfo($_FILES["media_file"]["name"], PATHINFO_EXTENSION));
 
     // Check file size (you can adjust the size limit)
     if ($_FILES["media_file"]["size"] > 52428800) {
         errorResponse(400, 'Sorry, your file is too large.');
+        exit;
     }
 
     // Allow certain file formats
-    $allowedExtensions = array("jpg", "jpeg", "png");
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif", "webp");
 
-    if (!in_array($imageFileType, $allowedExtensions)) {
+    if (!in_array($media_file_ext, $allowedExtensions)) {
         errorResponse(400, 'File type not supported.');
+        exit;
     }
 
     // Check if file already exists
     if (file_exists($target_file)) {
         errorResponse(500, 'Error when creating post:' . $target_file . ' already exists.');
+        exit;
     }
 
-    // Convert the image to JPEG format before moving it
-    $image = null;
-    switch ($imageFileType) {
-        case "jpg":
-        case "jpeg":
-            $image = imagecreatefromjpeg($_FILES["media_file"]["tmp_name"]);
-            break;
-        case "png":
-            $image = imagecreatefrompng($_FILES["media_file"]["tmp_name"]);
-            break;
-        // case "gif":
-        //     $image = imagecreatefromgif($_FILES["media_file"]["tmp_name"]);
-        //     break;
-        // case "webp":
-        //     $image = imagecreatefromwebp($_FILES["media_file"]["tmp_name"]);
-        //     break;
+    if (move_uploaded_file($_FILES["media_file"]["tmp_name"], $target_file) === false) {
+        errorResponse(500, 'Error when Moving to uploads directory: ' . $target_file);
+        exit;
     }
 
-    if ($image !== null) {
-        // Save the image as jpeg
-        imagejpeg($image, $target_file);
-        imagedestroy($image);
-    } else {
-        errorResponse(500, 'Failed to process the image.');
-    }
 } else {
     // Check if the post text and media are not empty together
     if (empty($content)) {
         errorResponse(400, 'Empty post not allowed.');
+        exit;
     }
     $media_file_id = null;
     $media_file_ext = null;
@@ -96,11 +80,12 @@ $userId = $_SESSION['user_id'];
 if (!createPost($conn, $userId, $content, $media_file_id, $media_file_ext)) {
     if ($target_file) {
         if (unlink($target_file) === false) {
-            error_log('Error: Failed to delete ' . $target_file);
+            error_log('Error: Failed to delete untracked file: ' . $target_file);
         }
     }
     $conn->close();
     errorResponse(500, 'Error when creating post');
+    exit;
 }
 
 $conn->close();
