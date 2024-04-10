bf30ae9d98e9c54224e6d331f332bc850e64b63f
prc16 <81236125+prc16@users.noreply.github.com>
2024-04-10 22:55:43 +0530

    added link to bottom-right

--- whisper/bottom-left/content.php
+++ whisper/bottom-left/content.php
@@ -1,7 +1,9 @@
 <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/php/userDetails.php'; ?>
 <div id="bottom-left-container">
-    <div id="bottom-left-profile-container">
-        <img id="bottom-left-profile-picture" class="profile-picture" src="<?= htmlspecialchars($profilePicture) ?>">
-        <div id="bottom-left-profile-username" class="profile-username"><?= htmlspecialchars($username) ?></div>
-    </div>
+    <a href="/u/<?= htmlspecialchars($username) ?>" class="username_link">
+        <div id="bottom-left-profile-container">
+            <img id="bottom-left-profile-picture" class="profile-picture" src="<?= htmlspecialchars($profilePicture) ?>">
+            <div id="bottom-left-profile-username" class="profile-username"><?= htmlspecialchars($username) ?></div>
+        </div>
+    </a>
 </div>
