5571ebec9d210c8fcb4a9ab10ae9c8218a558adb
prc16 <81236125+prc16@users.noreply.github.com>
2024-04-10 14:03:42 +0530

    profile page bug fix

--- whisper/profile/content.php
+++ whisper/profile/content.php
@@ -15,33 +15,33 @@
         </div>
         <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/whisper/update_profile/content.php'; ?>
         <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/whisper/update_username/content.php'; ?>
+        <script>
+            document.addEventListener('DOMContentLoaded', () => {
+
+                document.getElementById('updateProfileButton').addEventListener("click", updateProfilePicture);
+                document.getElementById('updateUsernameButton').addEventListener("click", updateUsername);
+
+                function updateProfilePicture() {
+                    // Add functionality to update profile picture
+                    // window.location.href = '../update_profile/';
+                    // Trigger event
+                    let inputImage = document.getElementById('inputImage');
+
+                    // Check if a file has already been selected
+                    if (inputImage.files.length === 0) {
+                        inputImage.click();
+                    }
+                    document.getElementById('updateUsernameContainer').style.display = 'none';
+                    document.getElementById('updateProfileContainer').style.display = 'flex';
+                }
+
+                function updateUsername() {
+                    // Add functionality to update username
+                    // window.location.href = '../update_username/';
+                    document.getElementById('updateProfileContainer').style.display = 'none';
+                    document.getElementById('updateUsernameContainer').style.display = 'block';
+                }
+            });
+        </script>
     <?php endif; ?>
 </div>
-<script>
-    document.addEventListener('DOMContentLoaded', () => {
-
-        document.getElementById('updateProfileButton').addEventListener("click", updateProfilePicture);
-        document.getElementById('updateUsernameButton').addEventListener("click", updateUsername);
-
-        function updateProfilePicture() {
-            // Add functionality to update profile picture
-            // window.location.href = '../update_profile/';
-            // Trigger event
-            let inputImage = document.getElementById('inputImage');
-
-            // Check if a file has already been selected
-            if (inputImage.files.length === 0) {
-                inputImage.click();
-            }
-            document.getElementById('updateUsernameContainer').style.display = 'none';
-            document.getElementById('updateProfileContainer').style.display = 'flex';
-        }
-
-        function updateUsername() {
-            // Add functionality to update username
-            // window.location.href = '../update_username/';
-            document.getElementById('updateProfileContainer').style.display = 'none';
-            document.getElementById('updateUsernameContainer').style.display = 'block';
-        }
-    });
-</script>
