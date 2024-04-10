db18fc485e38947c0b008091ea11228f32cdf639
prc16 <81236125+prc16@users.noreply.github.com>
2024-04-10 15:30:34 +0530

    sidebar css fix

--- whisper/bottom-left/content.php
+++ whisper/bottom-left/content.php
@@ -4,12 +4,4 @@
         <img id="bottom-left-profile-picture" class="profile-picture" src="<?= htmlspecialchars($profilePicture) ?>">
         <div id="bottom-left-profile-username" class="profile-username"><?= htmlspecialchars($username) ?></div>
     </div>
-    <div id="bottom-left-buttons-container">
-        <?php if ($loggedIn): ?>
-            <a id="bottom-left-buttons-logout" class="logout-btn" href="/whisper/logout/logout.php">Logout</a>
-        <?php else: ?>
-            <a id="bottom-left-buttons-login" class="login-btn" href="/whisper/login/">Login</a>
-            <a id="bottom-left-buttons-signup" class="signup-btn" href="/whisper/signup/">Signup</a>
-        <?php endif; ?>
-    </div>
 </div>
--- whisper/sidebar-left/content.php
+++ whisper/sidebar-left/content.php
@@ -1,3 +1,4 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/php/userDetails.php'; ?>
 <div class="sidebar-container">
     <nav class="imp-links">
         <a href="/"><i class="fab fa-discourse"></i><b>Whisper</b></a>
@@ -5,6 +6,12 @@
         <a href="/profile"><i class="far fa-user"></i>Profile</a>
         <a href="#"><i class="far fa-envelope"></i>Messages</a>
         <a href="#"><i class="far fa-user-friends"></i>Following</a>
+        <?php if ($loggedIn) : ?>
+            <a href="/logout"><i class="far fa-sign-out"></i>Log Out</a>
+        <?php else : ?>
+            <a href="/login"><i class="far fa-sign-in-alt"></i>Log In</a>
+            <a href="/signup"><i class="far fa-user-plus"></i>Sign Up</a>
+        <?php endif; ?>
     </nav>
     <div class="bottom-left">
         <?php include_once $_SERVER['DOCUMENT_ROOT'] . "/whisper/bottom-left/content.php"; ?>
