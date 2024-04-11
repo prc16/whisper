b2cda365fb019c739856b15a998ebc7ef9c3c683
prc16 <81236125+prc16@users.noreply.github.com>
2024-04-11 11:58:04 +0530

    css for sidebar-right

---
+++ whisper/user_posts/content.php
@@ -0,0 +1,119 @@
+ <!-- Display existing posts -->
+ <div id="postsFeedContainer"></div>
+ <script>
+     document.addEventListener('DOMContentLoaded', () => {
+         const postsFeedContainer = document.getElementById('postsFeedContainer');
+
+         // Function to display posts
+         function displayPosts(posts) {
+             if (!postsFeedContainer) {
+                 console.error("Posts container not found");
+                 return;
+             }
+             postsFeedContainer.innerHTML = '';
+             posts.forEach(post => {
+                 const postElement = document.createElement('div');
+                 postElement.className = 'post';
+                 postElement.innerHTML = `
+            <div class="displayPostContainer">
+                <div id="displayPostContainerPart1">
+                    <a href="/u/${post.username}" class="profile_link"><img src="${post.profile_file_path}" class="profile-picture" alt=""></a>
+                </div>
+                <div id="displayPostContainerPart2">
+                    <a href="/u/${post.username}" class="username_link"><h2 class="profile-username">${post.username}</h2></a>
+                    <p>${post.post_text}</p>
+                    <div id="displayPostMediaPreview">
+                    <img src="${post.post_file_path}" alt="" class="image-preview">
+                    </div>
+                    <div id="displayPostButtons">
+                        
+                        <div class="voteCount">Votes: ${post.vote_count}</div>
+                    
+                        <label for="upvote${post.post_id}" class="btn btn2 ${post.vote_type === 'upvote' ? 'btn-selected' : ''}" title="Upvote"><i class="fas fa-arrow-alt-up"></i></label>
+                        <button id="upvote${post.post_id}" class="vote-btn hidden" data-id="${post.post_id}" data-type="upvote"></button>
+        
+                        
+                        <label for="downvote${post.post_id}" class="btn btn2 ${post.vote_type === 'downvote' ? 'btn-selected' : ''}" title="Downvote"><i class="fas fa-arrow-alt-down"></i></label>
+                        <button id="downvote${post.post_id}" class="vote-btn hidden" data-id="${post.post_id}" data-type="downvote"></button>
+                    </div>
+                </div>
+            </div>
+        `;
+                 postsFeedContainer.appendChild(postElement);
+             });
+         }
+
+
+         // Function to handle voting
+         function vote(event) {
+             const {
+                 target
+             } = event;
+             if (target.classList.contains('vote-btn')) {
+                 const type = target.dataset.type;
+                 const postId = target.dataset.id;
+
+                 const formData = new FormData();
+                 formData.append('action', type);
+                 formData.append('post_id', postId);
+
+                 fetch('/whisper/posts/server.php', {
+                         method: 'POST',
+                         body: formData
+                     })
+                     .then(response => {
+                         if (response.status === 200) {
+                             return response.json();
+                         } else if (response.status === 401) {
+                             alert('You need to log in to vote.');
+                             throw new Error('Unauthorized');
+                         } else {
+                             throw new Error(`Error: Status ${response.status}`);
+                         }
+                     })
+                     .then(posts => {
+                         // FINSHME: only update vote count
+                         displayPosts(posts);
+                     })
+                     .catch(error => {
+                         console.error('Error:', error.message);
+                     });
+             }
+         }
+
+
+         // Event listener for voting
+         document.addEventListener('click', vote);
+
+         function fetchPosts() {
+             fetch('/whisper/posts/server.php')
+                 .then(response => {
+                     if (!response.ok) {
+                         throw new Error('Network response was not ok');
+                     }
+                     return response.json();
+                 })
+                 .then(posts => {
+                     displayPosts(posts);
+                 })
+                 .catch(error => {
+                     console.error('Fetch error:', error);
+                 });
+         }
+
+         // Fetch posts initially
+         fetchPosts();
+
+         // Fetch posts every 5 seconds
+         // setInterval(fetchPosts, 5000);
+
+
+         // Function to handle the 'updateNeeded' event
+         function handleUpdateEvent() {
+             fetchPosts();
+         }
+
+         // Add event listener for 'update' event on displayPosts div
+         postsFeedContainer.addEventListener("updateNeeded", handleUpdateEvent);
+     });
+ </script>
---
+++ whisper/user_posts/server.php
@@ -0,0 +1,61 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'] . '/php/all.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/php/database.php';
+
+$conn = getConnection();
+if(!$conn) {
+    serverMaintenanceResponse();
+    exit;
+}
+
+session_start();
+
+// Validate request method
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Validate session and action
+    if (!isset($_SESSION['user_id'])) {
+        http_response_code(401); // Unauthorized
+        $conn->close();
+        exit();
+    }
+
+    $userId = $_SESSION['user_id'];
+
+    // Disable autocommit to start a new transaction
+    $conn->autocommit(false);
+
+    try {
+        // Validate form data
+        $action = $_POST['action'] ?? null;
+        $postId = $_POST['post_id'] ?? null;
+        if (!$action || !in_array($action, ['upvote', 'downvote']) || !$postId || strlen($postId) !== 16) {
+            throw new InvalidArgumentException("Invalid action or post ID provided.");
+        }
+
+        handleVote($conn, $userId, $postId, $action);
+
+        // Commit the transaction if no exceptions occur
+        $conn->commit();
+    } catch (Exception $e) {
+        $conn->rollback();
+        handleException($e);
+    } finally {
+        // Re-enable autocommit
+        $conn->autocommit(true);
+    }
+}
+
+// Set character encoding to UTF-8
+header('Content-Type: application/json; charset=utf-8');
+
+// Return posts as JSON
+if (isset($_SESSION['user_id'])) {
+    echo json_encode(getPostsWithVotes($conn, $_SESSION['user_id']));
+} else {
+    echo json_encode(getPosts($conn));
+}
+
+// Close the database connection
+$conn->close();
